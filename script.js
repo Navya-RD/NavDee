c3fe00c68271435ea5ff690fcab3320793567063
Navya-RD <navyardbtech25@rvu.edu.in>
2025-12-14 14:25:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function validateForm() {
+  let name = document.getElementById("name").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let message = document.getElementById("message").value.trim();
+
+  if (name === "" || email === "" || message === "") {
+    alert("Please fill out all fields.");
+    return false;
+  }
+  alert("Thank you for contacting NavDee!");
+  return true;
+}
